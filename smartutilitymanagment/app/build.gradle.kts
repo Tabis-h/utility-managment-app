105c60489568f53015bca629fe4a39fbfda9193d
Tabis-h <tabishsubedar50@gmail.com>
2024-10-30 21:24:43 +0530

    123

--- smartutilitymanagment/app/build.gradle.kts
+++ smartutilitymanagment/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
     id("com.google.gms.google-services")
 }
 
@@ -15,7 +14,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -38,15 +36,24 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase dependencies
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database-ktx:20.0.5")
+
 }
+
+// Apply the google-services plugin
+apply(plugin = "com.google.gms.google-services")
+
+
--- smartutilitymanagment/app/src/main/java/com/example/smartutilitymanagment/singupActivity.kt
+++ smartutilitymanagment/app/src/main/java/com/example/smartutilitymanagment/singupActivity.kt
@@ -1,20 +1,58 @@
 package com.example.smartutilitymanagment
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class singupActivity : AppCompatActivity() {
+
+class SignupActivity : AppCompatActivity() {
+    var signupName: EditText? = null
+    var signupUsername: EditText? = null
+    var signupEmail: EditText? = null
+    var signupPassword: EditText? = null
+    var loginRedirectText: TextView? = null
+    var signupButton: Button? = null
+    var database: FirebaseDatabase? = null
+    var reference: DatabaseReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_singup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        signupName = findViewById<EditText>(R.id.signup_name)
+        signupEmail = findViewById(R.id.signup_email)
+        signupUsername = findViewById<EditText>(R.id.signup_username)
+        signupPassword = findViewById<EditText>(R.id.signup_password)
+        loginRedirectText = findViewById(R.id.loginRedirectText)
+        signupButton = findViewById(R.id.signup_button)
+        signupButton.setOnClickListener(View.OnClickListener {
+            database = FirebaseDatabase.getInstance()
+            reference = database!!.getReference("users")
+            val name = signupName.getText().toString()
+            val email = signupEmail.getText().toString()
+            val username = signupUsername.getText().toString()
+            val password = signupPassword.getText().toString()
+            val helperClass: HelperClass = HelperClass(name, email, username, password)
+            reference!!.child(username).setValue(helperClass)
+            Toast.makeText(this@SignupActivity, "You have signup successfully!", Toast.LENGTH_SHORT)
+                .show()
+            val intent = Intent(
+                this@SignupActivity,
+                LoginActivity::class.java
+            )
+            startActivity(intent)
+        })
+        loginRedirectText.setOnClickListener(View.OnClickListener {
+            val intent = Intent(
+                this@SignupActivity,
+                LoginActivity::class.java
+            )
+            startActivity(intent)
+        })
     }
 }
